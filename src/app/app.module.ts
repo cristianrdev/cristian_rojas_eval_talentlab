99b1927d6e25fa490de4b5e184d35b534d1f7a33
Your Name <crrojasserrano@gmail.com>
2021-09-24 12:03:45 -0300

    componentes añadidos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './compartida/header/header.component';
+import { AsideComponent } from './compartida/aside/aside.component';
+import { FooterComponent } from './compartida/footer/footer.component';
+import { CarruselComponent } from './compartida/carrusel/carrusel.component';
+import { ProductosComponent } from './compartida/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    FooterComponent,
+    CarruselComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule
